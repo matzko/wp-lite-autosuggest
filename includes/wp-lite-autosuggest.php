de3d9edcf8d1779f92782c0d1e960484f116e651
Austin Matzko <austin@pressedcode.com>
2011-03-16 16:41:22 -0500

    add event_init method

--- includes/wp-lite-autosuggest.php
+++ includes/wp-lite-autosuggest.php
@@ -4,6 +4,18 @@ class WP_Lite_Autosuggest_Control
 {
 	public function __construct()
 	{
+		if ( ! function_exists( 'load_wp_json_rpc_api' ) ) {
+			include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'wp-json-rpc-api' . DIRECTORY_SEPARATOR . 'wp-json-rpc-api.php';
+		}
+
+		load_wp_json_rpc_api();
+
+		add_action( 'init', array($this, 'event_init' ) );
+	}
+
+	public function event_init()
+	{
+
 	}
 }
 
