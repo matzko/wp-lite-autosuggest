5115b43fbaa849115279578af87ed20bc4cd6aa8
Austin Matzko <austin@pressedcode.com>
2011-03-16 17:13:17 -0500

    introduce the query object

--- includes/wp-lite-autosuggest.php
+++ includes/wp-lite-autosuggest.php
@@ -12,6 +12,41 @@ class WP_Lite_Autosuggest_Control
 
 		load_wp_json_rpc_api();
 	}
+
+	/**
+	 * Get search results from query.
+	 *
+	 * @param string $query The search query.
+	 * @param array $extras Optional. The submitted data in an associated array.
+	 * @return The results.
+	 */
+	public function get_results_from_query( $query = '', $extras = array() )
+	{
+		
+	}
+}
+
+class WP_Lite_Autosuggest_Model
+{
+	/**
+	 * Create a query object from the submitted extra data.
+	 *
+	 * @param array $data The submitted data about this query.
+	 * @return WP_Lite_Autosuggest_Query The query object reflecting this query.
+	 */
+	public function build_query( $data = array() )
+	{
+		$query = new WP_Lite_Autosuggest_Query;	
+	}
+}
+
+/**
+ * A query object
+ */
+class WP_Lite_Autosuggest_Query
+{
+	
+
 }
 
 function load_wp_lite_autosuggest()
