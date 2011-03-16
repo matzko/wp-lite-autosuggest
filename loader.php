65004aeb4f6b68108dae3a7bb82b43d19a27f07e
Austin Matzko <austin@pressedcode.com>
2011-03-16 10:28:18 -0500

    using a loader to check for php 5 compat

---
+++ loader.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: WordPress Lite Autosuggest
+Plugin URI:
+Description: Provide a lightweight autosuggest tool for WordPress.
+Version: 1.0
+Author: Austin Matzko
+Author URI: http://austinmatzko.com
+
+Copyright 2011 Austin Matzko
+
+*/
+
+class WP_Lite_Autosuggest_Control
+{
+
+}
+
