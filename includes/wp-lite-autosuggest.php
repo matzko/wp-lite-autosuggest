ddb15587596f4df2a2eb81e489e2693181e43e1b
Austin Matzko <austin@pressedcode.com>
2011-03-16 12:16:54 -0500

    initialize main class for auto-complete

--- includes/wp-lite-autosuggest.php
+++ includes/wp-lite-autosuggest.php
@@ -2,5 +2,16 @@
 
 class WP_Lite_Autosuggest_Control
 {
-
+	public function __construct()
+	{
+	}
 }
+
+function load_wp_lite_autosuggest()
+{
+	global $wp_lite_autosuggest;
+	$wp_lite_autosuggest = new WP_Lite_Autosuggest_Control;
+}
+
+add_action( 'plugins_loaded', 'load_wp_lite_autosuggest' );
+// eof
