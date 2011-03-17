4a08d0064eafe28dafbec3dc4f71edb9b68f876f
Austin Matzko <austin@pressedcode.com>
2011-03-17 05:01:35 -0500

    iterator and link objects introduced

--- includes/wp-lite-autosuggest.php
+++ includes/wp-lite-autosuggest.php
@@ -2,8 +2,11 @@
 
 class WP_Lite_Autosuggest_Control
 {
+	public $model; 
 	public function __construct()
 	{
+		$this->model = new WP_Lite_Autosuggest_Model;
+
 		if ( ! function_exists( 'load_wp_json_rpc_api' ) ) {
 			include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'wp-json-rpc-api' . DIRECTORY_SEPARATOR . 'wp-json-rpc-api.php';
 		}
@@ -22,7 +25,8 @@ class WP_Lite_Autosuggest_Control
 	 */
 	public function get_results_from_query( $query = '', $extras = array() )
 	{
-		
+		$query_obj = $this->model->build_query( $extras );	
+		$query_obj->query = $query;
 	}
 }
 
@@ -37,6 +41,18 @@ class WP_Lite_Autosuggest_Model
 	public function build_query( $data = array() )
 	{
 		$query = new WP_Lite_Autosuggest_Query;	
+		return $query;
+	}
+
+	/**
+	 * Perform a query using the query object.
+	 *
+	 * @param WP_Lite_Autosuggest_Query $qry The query object.
+	 * @return WP_Lite_Autosuggest_Query_Results object.
+	 */
+	public function perform_query( WP_Lite_Autosuggest_Query $qry )
+	{
+		$results = new WP_Lite_Autosuggest_Query_Results; 
 	}
 }
 
@@ -45,6 +61,11 @@ class WP_Lite_Autosuggest_Model
  */
 class WP_Lite_Autosuggest_Query
 {
+	/**
+	 * The text to search for
+	 */
+	public $query = '';
+
 	/**
 	 * Reflecting the type of query: 'post', 'term', 'user', or 'custom'
 	 */
@@ -55,6 +76,78 @@ class WP_Lite_Autosuggest_Query
 	{
 
 	}
+}
+
+class WP_Lite_Autosuggest_Query_Link
+{
+	public $url;
+	public $excerpt;
+	public $content;
+	public $title;
+}
+
+/**
+ * Iterate over WP_Lite_Autosuggest_Query_Link objects
+ */
+class WP_Lite_Autosuggest_Query_Results implements Iterator
+{
+	private $_objects;
+	private $_position = 0;
+
+	public function add_result( WP_Lite_Autosuggest_Query_Link $result )
+	{
+		$this->_objects[] = $result;
+	}
+
+	/**
+	 * Iterator-required methods
+	 */
+
+	/**
+	 * Return the current content object.
+	 *
+	 * @return result
+	 */
+	public function current()
+	{
+		return $this->_objects[$this->_position];
+	}
+
+	/**
+	 * @return scalar
+	 */
+	public function key()
+	{
+		return $this->_position;
+	}
+
+	/**
+	 * @return void 
+	 */
+	public function next()
+	{
+		$this->_position++;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function rewind()
+	{
+		$this->_position = 0;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function valid()
+	{
+		return ( isset($this->_objects[$this->_position]) && $this->_objects[$this->_position] instanceof WP_Lite_Autosuggest_Query_Link );
+	}
+	
+	/**
+	 * End required iterator methods
+	 */
 }
 
 function load_wp_lite_autosuggest()
